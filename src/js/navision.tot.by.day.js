45747083ae35f8ca2319bcc340abb0261861ed8d
Andrea Samorini <samorini@gmail.com>
2021-09-10 09:20:33 +0200

    Update navision.tot.by.day.js

--- src/js/navision.tot.by.day.js
+++ src/js/navision.tot.by.day.js
@@ -2,7 +2,19 @@ var rows=jQuery('#listaRigheOdT tr:not(.jqgfirstrow)'),
 	ggOld='',
 	hoursTot=0,
 	previousColHoursTot,
-	alternateBackground=true;
+	alternateBackground=true,
+    	totalizePreviousDay=function(hoursTotal,colHoursToUpdate){	//aggiorno il totale ore precedente
+		var totEl=colHoursToUpdate.find('.totHours');
+		if (!totEl.length){
+			totEl=jQuery('<div/>',{'class':'totHours','style':'font-weight:bold;font-size:16px;text-align: left;'})
+					.appendTo(colHoursToUpdate);
+		}
+		
+		totEl
+		.html(hoursTotal)
+		.css('color',hoursTotal>8 ? 'black' : hoursTotal===8 ? 'green' : 'red');
+		colHoursToUpdate.parents('tr').css('border-bottom','3px solid #000');
+	};
 
 rows.each(function(index){
 	var row=jQuery(this),
@@ -11,36 +23,38 @@ rows.each(function(index){
 		colHours=cols.filter('[aria-describedby=listaRigheOdT_OreOrdinarie]'),
 		gg=colDay.html(),
 		hours=+colHours.html().replace(',','.'),
-		isLastRow=rows.length==index+1,
-		totEl;
+		isLastRow=rows.length==index+1;
 		
 	//AGGIORNAMENTO RIGHE PRECEDENTI
 	if ((gg!==ggOld && ggOld) || isLastRow){
-		//aggiorno il totale ore precedente
+		if (gg!==ggOld && ggOld){
+			totalizePreviousDay(hoursTot,previousColHoursTot);
+			hoursTot=0;
+		}
 		if (isLastRow){
+			hoursTot+=hours;
 			previousColHoursTot=cols.filter('[aria-describedby=listaRigheOdT_OreStraordinarie]');
+			totalizePreviousDay(hoursTot,previousColHoursTot);
 		}
-		totEl=previousColHoursTot.find('.totHours');
-		if (!totEl.length){
-			totEl=jQuery('<div/>',{'class':'totHours','style':'font-weight:bold;font-size:16px;text-align: left;'})
-					.appendTo(previousColHoursTot);
-		}
-		totEl.html(isLastRow ? hoursTot+hours : hoursTot);
 	}
 	
 	//IDENTIFICAZIONE RIGA ATTUALE
 	if (gg===ggOld){
-		hoursTot+=hours;
+		if (!isLastRow){
+			hoursTot+=hours;
+		}
 
 	}else{
 		ggOld=gg;
 		hoursTot=hours;	//conteggio nuovo totale ore
 		alternateBackground=!alternateBackground;
 	}
+	/*
 	if (alternateBackground){
 		colHours
 		.add(colDay)
 		.css('background','#ffeded');
 	}
+	*/
 	previousColHoursTot=cols.filter('[aria-describedby=listaRigheOdT_OreStraordinarie]');
 });
